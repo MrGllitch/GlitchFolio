6b07993d58441346e92379379913d65e0d6dcb14
Matěj K <hobitekCZ@post.cz>
2024-07-25 23:01:56 +0200

    Android privacy policy

---
+++ src/routes/privacy_policy/+page.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Zásady ochrany soukromí</h1>
+
+<h2>1. Úvod</h2>
+<p>
+	Tyto zásady ochrany soukromí popisují, jak aplikace "Můj sklad" (dále jen "aplikace") chrání
+	soukromí svých uživatelů. Uživatelé jsou vyzváni, aby si tyto zásady pečlivě přečetli. Používáním
+	naší aplikace souhlasíte s podmínkami těchto zásad ochrany soukromí.
+</p>
+
+<h2>2. Kontakt na správce údajů</h2>
+<p>Správcem osobních údajů je:</p>
+<ul>
+	<li>Jméno: Matěj Kratochvíl</li>
+	<li>Adresa: Příčná 849, Chrudim</li>
+	<li>E-mail: <a href="mailto:matej.kratochvil711@gmail.com">matej.kratochvil711@gmail.com</a></li>
+	<li>Telefon: +420 777 124 866</li>
+</ul>
+
+<h2>3. Shromažďované údaje</h2>
+<p>Naše aplikace "Můj sklad" nesbírá žádné osobní údaje uživatelů.</p>
+
+<h2>4. Účel shromažďování údajů</h2>
+<p>
+	Vzhledem k tomu, že aplikace nesbírá žádné osobní údaje, nejsou žádné údaje shromažďovány pro
+	jakýkoli účel.
+</p>
+
+<h2>5. Sdílení údajů s třetími stranami</h2>
+<p>Vaše osobní údaje nesdílíme s žádnými třetími stranami, protože žádné údaje nesbíráme.</p>
+
+<h2>6. Ochrana a bezpečnost údajů</h2>
+<p>
+	I když naše aplikace nesbírá žádné osobní údaje, věnujeme pozornost bezpečnosti a ochraně všech
+	ostatních dat, které aplikace zpracovává.
+</p>
+
+<h2>7. Uchovávání údajů</h2>
+<p>
+	Vzhledem k tomu, že aplikace nesbírá žádné osobní údaje, nedochází k žádnému uchovávání osobních
+	údajů.
+</p>
+
+<h2>8. Práva uživatelů</h2>
+<p>
+	Jelikož aplikace "Můj sklad" nesbírá žádné osobní údaje, uživatelé nemají žádná specifická práva v
+	souvislosti s osobními údaji, které by se mohly na naši aplikaci vztahovat.
+</p>
+
+<h2>9. Změny v zásadách ochrany soukromí</h2>
+<p>
+	Tyto zásady ochrany soukromí mohou být čas od času aktualizovány. O všech změnách vás budeme
+	informovat prostřednictvím aktualizací naší aplikace. Doporučujeme pravidelně kontrolovat tyto
+	zásady, abyste byli informováni o jakýchkoli změnách.
+</p>
+
+<h2>10. Kontakt</h2>
+<p>
+	Pokud máte jakékoli dotazy nebo připomínky týkající se těchto zásad ochrany soukromí, můžete nás
+	kontaktovat na:
+</p>
+<ul>
+	<li>E-mail: <a href="mailto:matej.kratochvil711@gmail.com">matej.kratochvil711@gmail.com</a></li>
+	<li>Telefon: +420 777 124 866</li>
+</ul>
+
+<style>
+	* {
+		font-family: Arial, sans-serif;
+		line-height: 1.6;
+		margin: 20px;
+	}
+	h1,
+	h2 {
+		color: #333;
+	}
+	p {
+		margin: 10px 0;
+	}
+</style>
